2aeba3fb7bacd821e36ac9015880719f4670383c
Nastya-Leto <soidberg91@gmail.com>
2024-10-13 08:34:15 +0400

    Добавление изменений по 1-ой части ДЗ

--- tests/test_fill_form.py
+++ tests/test_fill_form.py
@@ -1,22 +1,21 @@
-from data.test_data import path, first_name, last_name, email, phone_number, birthday, subjects, current_address, \
-    download_path_picture
 from pages.registration_page import RegistrationPage
 
 
 def test_fill_form():
     registration_page = RegistrationPage()
+    download_picture = rf"C:\Users\user\PycharmProjects\HW_selene_9\resources\image.jpg"
 
-    registration_page.open(path)
-    registration_page.fill_first_name(first_name)
-    registration_page.fill_last_name(last_name)
-    registration_page.fill_user_email(email)
+    registration_page.open('/automation-practice-form')
+    registration_page.fill_first_name('Анастасия')
+    registration_page.fill_last_name('З')
+    registration_page.fill_user_email('test@mail.ru')
     registration_page.fill_female()
-    registration_page.fill_user_number(phone_number)
-    registration_page.fill_date_of_birth(birthday["day"], birthday["month"], birthday["year"])
-    registration_page.fill_subjects(subjects)
+    registration_page.fill_user_number('7927000000')
+    registration_page.fill_date_of_birth(20,6,1995)
+    registration_page.fill_subjects('Arts')
     registration_page.fill_hobbies()
-    registration_page.fill_picture(download_path_picture)
-    registration_page.fill_current_address(current_address)
+    registration_page.fill_picture(download_picture)
+    registration_page.fill_current_address('Samara')
     registration_page.fill_state_and_city()
     registration_page.submit_button()
     registration_page.should_registered_user_with(
